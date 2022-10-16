fb64940a40ac00ddcd54015430733fa2e64a06c6
Gilang Ramadhan <gilang.ramadhan011297+github@gmail.com>
2022-10-16 16:01:29 +0700

    Updates

---
+++ Reading_multiple_lines/reading_multiple_lines.py
@@ -0,0 +1,16 @@
+# with open('data_anggaran.csv', 'r') as anggaran:
+#     anggaran = anggaran.readlines()
+
+# for data in anggaran:
+#     if "realisasi" in data:
+#         print(anggaran.rstrip())
+## gagal diatas nyoba nyoba, 
+##kalob code
+
+with open("emails.txt")  as emails:
+    emails = emails.readlines()
+
+for email in emails:
+    if "gmail" in email:
+        print(email.rstrip())
+        
---
+++ Writing_files/writing_files.py
@@ -0,0 +1,16 @@
+#how you write new file
+# with open('new_files.txt', 'w') as file:
+#     file.write("this is how you create new files in Python-201") #if we change this text it will replace the documents with newer command
+    # file.write("\n\tThis is tabbed!")
+
+#if you want append(add some text) to existing text this the syntax look like
+# with open('new_files.txt', 'a') as file:
+#     file.write(" this is append ny another code")
+
+#if you want add a new line this is the syntax
+# with open('new_files.txt', 'a') as file:
+#     file.write("\nSecond line")
+
+# if you want to add new line with tab in it this the syntax
+with open('new_files.txt', 'a') as file:
+    file.write("\n\tThis line tabbed")
